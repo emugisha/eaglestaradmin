3eb3ddce5c714fa915dfcb12041cbf1112065870
Eric Mugisha <erimug@gmail.com>
2017-01-03 21:25:19 -0500

    Added the main admin panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {PanelModule} from 'primeng/primeng';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RepairsComponent } from './repairs/repairs.component';
 import { RouterModule, Routes } from '@angular/router';
 import { PermissionsComponent } from './permissions/permissions.component';
+import {DataTableModule,SharedModule} from 'primeng/primeng';
 
 
 const appRoutes : Routes = [
@@ -29,7 +29,8 @@ const appRoutes : Routes = [
     FormsModule,
     HttpModule,
     RouterModule.forRoot(appRoutes),
-    PanelModule
+    SharedModule,
+    DataTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/repairs/repairs.component.ts
+++ src/app/repairs/repairs.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './repairs.component.html',
   styleUrls: ['./repairs.component.css']
 })
-export class RepairsComponent{
+export class RepairsComponent implements OnInit{
 
+  newBooking = [];
   constructor() { }
-
+  ngOnInit() {
+    this.newBooking[0] ={name:'Mr Bean England', city:'Silver Spring', zipCode:'20910', phone:'345-234-3456', availability:'12:45-12:34'};
+    this.newBooking[1] ={name:'Tekno Miles', city:'Rosslyn', zipCode:'20910', phone:'345-234-3456', availability:'2:00-12:9'}
+  }
+  
 
 }
