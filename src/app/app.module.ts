601094dd8a76c4b77a0f6177362bb8b346aa9be6
Eric Mugisha <en.mugisha@gmail.com>
2017-07-05 21:13:29 -0400

    Upload order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,23 @@ import { HomeComponent } from './home/home.component';
 import { RepairsComponent } from './repairs/repairs.component';
 import { RouterModule, Routes } from '@angular/router';
 import { PermissionsComponent } from './permissions/permissions.component';
-import {DataTableModule,SharedModule, DialogModule} from 'primeng/primeng';
+import {DataTableModule, SharedModule, DialogModule, FileUploadModule} from 'primeng/primeng';
 import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 import { LoginComponent } from './login/login.component';
 import {LoginService} from "./login/login.service";
 import {RouteGuard} from "./login/route-guard.service";
 import { HomeDropDownDirective } from './home/home-drop-down.directive';
 import {RepairService} from "./repairs/repair-service.service";
+import { ApplianceUploadComponent } from './appliance-upload/appliance-upload.component';
+import { OrdersComponent } from './orders/orders.component';
 
 const appRoutes : Routes = [
   {path:'', pathMatch: 'full', redirectTo:'/repairs'},
   {path:'repairs', component:RepairsComponent, canActivate:[RouteGuard]},
   {path:'permissions',component:PermissionsComponent, canActivate:[RouteGuard]},
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'upload',component:ApplianceUploadComponent, canActivate:[RouteGuard]},
+  {path:'orders',component:OrdersComponent, canActivate:[RouteGuard]},
 ];
 
 const firebaseConfig = {
@@ -38,7 +42,9 @@ const firebaseConfig = {
     RepairsComponent,
     PermissionsComponent,
     LoginComponent,
-    HomeDropDownDirective
+    HomeDropDownDirective,
+    ApplianceUploadComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
@@ -48,6 +54,7 @@ const firebaseConfig = {
     SharedModule,
     DataTableModule,
     DialogModule,
+    FileUploadModule,
     AngularFireModule.initializeApp(firebaseConfig,{
       provider: AuthProviders.Password,
       method: AuthMethods.Password,
